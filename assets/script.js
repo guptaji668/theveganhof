3ef549008c5375cceddec983053fd8300e798a6e
Rohan Gupta <122445469+guptaji668@users.noreply.github.com>
2023-05-23 17:32:39 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// var img = document.getElementById('images');
+
+// var slides=["pic1.png","pic2.png","pic4.png","pic5.png", "pic6.png","pic7.png"];
+
+// var Start=0;
+
+// function slider(){
+
+//     if(Start<slides.length){
+//         Start=Start+1;
+//     }
+//     else{
+//         Start=1;
+//     }
+//     console.log("img",img);
+//     const pic=slides[Start-1]
+//     // alert(pic)
+
+//     img.innerHTML = "<img src="+pic+">"
+    
+   
+   
+//     // console.log("img",img);
+   
+// }
+// setInterval(slider,2000);
+
+
+
